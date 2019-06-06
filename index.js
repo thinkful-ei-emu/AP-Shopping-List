d0cbabde9a8daf4591547c31a07ab4ba0e37c83d
Tara Patel <t.jeanne.patel@gmail.com>
2019-06-06 16:25:54 -0500

    fixed delegation problem

--- index.js
+++ index.js
@@ -7,28 +7,24 @@
 $(function() {
 
   $('#js-shopping-list-form').on('submit', function(event){
-    console.log();
+
     event.preventDefault();
     const foodItem = $( 'input' ).val();
-    console.log(foodItem);
     $('.shopping-list').append(
       `<li><span class="shopping-item">${foodItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`
     );
   });
-});
 
-$(function(){
-  $('.shopping-item-toggle').click(function(){
-
-    console.log($(this));
-    console.log($(this).closest('li').find('.shopping-item'));
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(item){
+    item.preventDefault();
     $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
   });
 
-  $('.shopping-item-delete').click(function() {
+  $('.shopping-list').on('click', '.shopping-item-delete', function() {
     this.closest('li').remove();
   });
 });
+
 
 
 
